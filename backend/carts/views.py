d218125a732cfcae8e281e138782f42b39e9cff5
daniilradin <radin-04@mail.ru>
2025-01-14 20:33:38 +0300

    Final

---
+++ backend/carts/views.py
@@ -0,0 +1,101 @@
+# Create your views here.
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from carts.models import Cart
+from carts.utils import get_user_carts
+
+from shop.models import Goods
+
+
+def cart_add(request):
+    product_id = request.POST.get("product_id")
+
+
+    product = Goods.objects.get(id=product_id)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1)
+
+    else:
+        carts = Cart.objects.filter(
+            session_key=request.session.session_key, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(
+                session_key=request.session.session_key, product=product, quantity=1)
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
+    }
+
+    return JsonResponse(response_data)
+
+
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+    try:
+        quantity = int(quantity)
+        if quantity <= 0:
+            raise ValueError("Quantity must be a positive integer.")
+    except (ValueError, TypeError):
+        return JsonResponse({"error": "Invalid quantity provided."}, status=400)
+
+    cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request)
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
+
+
+def cart_remove(request):
+    cart_id = request.POST.get("cart_id")
+    try:
+        cart = Cart.objects.get(id=cart_id)
+    except Cart.DoesNotExist:
+        return JsonResponse({"error": "Cart item does not exist."}, status=404)
+
+    quantity = cart.quantity
+    cart.delete()
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Товар удален",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+
+    return JsonResponse(response_data)
---
+++ backend/shop/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+class Categories(models.Model):
+    name=models.CharField(max_length=100,unique=True, verbose_name='Название')
+    slug=models.SlugField(max_length=200,unique=True, verbose_name='URL', blank=True, null=True)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+    def __str__(self):
+        return self.name
+
+class Goods(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='URL', blank=True, null=True)
+    photo = models.ImageField(upload_to='shop_goods', blank=True)
+    rating = models.FloatField(default=0)
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ("id",)
+
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
+
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+
+    def display_id(self):
+        return f"{self.id:05}"
+
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price * self.discount / 100, 2)
+
+        return self.price
