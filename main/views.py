b7c30c7b90fa3c0081451e7e5202fd54dbe684c4
daniilradin <radin-04@mail.ru>
2025-01-14 20:33:08 +0300

    Final

--- main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from shop.models import Categories
-from shop.models import Goods
-# Create your views here.
-def index(request):
-    categories = Categories.objects.all()
-    news = Goods.objects.all()
-    return render (request, 'main/index.html', {'news': news})
