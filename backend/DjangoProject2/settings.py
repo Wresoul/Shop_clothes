ae69ac933c1b07e6074a4ababcf546db5b288325
daniilradin <radin-04@mail.ru>
2025-01-17 04:25:43 +0300

    blablabla12

--- backend/DjangoProject2/settings.py
+++ backend/DjangoProject2/settings.py
@@ -35,11 +35,13 @@ SECRET_KEY = 'django-insecure-3q7npl0dc4j$2nb-j0988&Ffdsce5MAx4cdsw(_)xhi$fy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['77.223.100.17',
-                 '127.0.0.1',
-                 'localhost',
-                 'ksysha-one-love.ru',
-                 'www.ksysha-one-love.ru']
+ALLOWED_HOSTS = [#'77.223.100.17',
+                 #'127.0.0.1',
+                 #'localhost',
+                 #'ksysha-one-love.ru',
+                 #'www.ksysha-one-love.ru'
+    '*'
+]
 
 
 # Application definition
